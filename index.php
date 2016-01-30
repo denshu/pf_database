02da25104e80c5b9bf1418232e513f554128ff5e
dj <dj@denshuto.me>
2016-01-29 18:17:44 -0800

    cleaning up code in css and index
    lol triple indent

--- index.php
+++ index.php
@@ -73,61 +73,61 @@
 				    <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
 				      <div class="panel-body">
 				      		
-							  	<?php
+					  	<?php
 
-								require_once('config.php');
+						require_once('config.php');
 
-								// Selecting by gender
-								echo '<div class="form-group"><label for="select-gender">Gender</label><select class="form-control" id="select-gender"><option></option>';
-								$selection3 = "SELECT DISTINCT gender FROM characters ORDER BY gender";
-								$result3 = mysqli_query($con,$selection3);
-								$row3 = mysqli_fetch_array($result3,MYSQLI_ASSOC);
-									echo '<option>',$row3["gender"],'</option>';
-								while($row3 = mysqli_fetch_array($result3,MYSQLI_ASSOC)) {
-									echo '<option>',$row3["gender"],'</option>';
-								}
-								echo '</select></div>';
+						// Selecting by gender
+						echo '<div class="form-group"><label for="select-gender">Gender</label><select class="form-control" id="select-gender"><option></option>';
+						$selection3 = "SELECT DISTINCT gender FROM characters ORDER BY gender";
+						$result3 = mysqli_query($con,$selection3);
+						$row3 = mysqli_fetch_array($result3,MYSQLI_ASSOC);
+							echo '<option>',$row3["gender"],'</option>';
+						while($row3 = mysqli_fetch_array($result3,MYSQLI_ASSOC)) {
+							echo '<option>',$row3["gender"],'</option>';
+						}
+						echo '</select></div>';
 
-								// Selecting by nation
-								echo '<div class="form-group"><label for="select-nation">Nation</label><select class="form-control" id="select-nation"><option></option>';
-								$selection = "SELECT DISTINCT nation FROM characters ORDER BY nation";
-								$result = mysqli_query($con,$selection);
-								$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-									echo '<option>',$row["nation"],'</option>';
-								while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
-									echo '<option>',$row["nation"],'</option>';
-								}
-								echo '</select></div>';
+						// Selecting by nation
+						echo '<div class="form-group"><label for="select-nation">Nation</label><select class="form-control" id="select-nation"><option></option>';
+						$selection = "SELECT DISTINCT nation FROM characters ORDER BY nation";
+						$result = mysqli_query($con,$selection);
+						$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+							echo '<option>',$row["nation"],'</option>';
+						while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
+							echo '<option>',$row["nation"],'</option>';
+						}
+						echo '</select></div>';
 
-								// Selecting by location
-								echo '<div class="form-group"><label for="select-location">Location</label><select class="form-control" id="select-location"><option></option>';
-								$selection2 = "SELECT DISTINCT location FROM characters ORDER BY location";
-								$result2 = mysqli_query($con,$selection2);
-								$row2 = mysqli_fetch_array($result2,MYSQLI_ASSOC);
-									echo '<option>',$row2["location"],'</option>';
-								while($row2 = mysqli_fetch_array($result2,MYSQLI_ASSOC)) {
-									echo '<option>',$row2["location"],'</option>';
-								}
-								echo '</select></div>';
+						// Selecting by location
+						echo '<div class="form-group"><label for="select-location">Location</label><select class="form-control" id="select-location"><option></option>';
+						$selection2 = "SELECT DISTINCT location FROM characters ORDER BY location";
+						$result2 = mysqli_query($con,$selection2);
+						$row2 = mysqli_fetch_array($result2,MYSQLI_ASSOC);
+							echo '<option>',$row2["location"],'</option>';
+						while($row2 = mysqli_fetch_array($result2,MYSQLI_ASSOC)) {
+							echo '<option>',$row2["location"],'</option>';
+						}
+						echo '</select></div>';
 
-								mysqli_free_result($result);
-								mysqli_free_result($result2);
-								mysqli_free_result($result3);
-								?>
-								<!-- Sorting method -->
-								<div class="form-group"><label for="sort-by">Sort By</label><select class="form-control" id="sort-by">
-								<option>name</option>
-								<option>hp_rating</option>
-								<option>sp_rating</option>
-								<option>str_rating</option>
-								<option>dex_rating</option>
-								<option>agi_rating</option>
-								<option>int_rating</option>
-								</select></div>
+						mysqli_free_result($result);
+						mysqli_free_result($result2);
+						mysqli_free_result($result3);
+						?>
+						<!-- Sorting method -->
+						<div class="form-group"><label for="sort-by">Sort By</label><select class="form-control" id="sort-by">
+						<option>name</option>
+						<option>hp_rating</option>
+						<option>sp_rating</option>
+						<option>str_rating</option>
+						<option>dex_rating</option>
+						<option>agi_rating</option>
+						<option>int_rating</option>
+						</select></div>
 
-								<button type="button" class="btn btn-default btn-lg" id="search-button">
-								  <span class="glyphicon glyphicon-search" aria-hidden="true"></span> Search
-								</button>
+						<button type="button" class="btn btn-default btn-lg" id="search-button">
+						  <span class="glyphicon glyphicon-search" aria-hidden="true"></span> Search
+						</button>
 				      </div>
 				    </div>
 				  </div>
