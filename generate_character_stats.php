fb5bef5e825145f6ec2d2e7ac83780deaf668fca
dj <dj@denshuto.me>
2017-10-12 11:01:26 -0700

    clean up code, add chart.js, prep for adding statistics feature
    probably more cleaning up to do but this mostly takes care of sloppy
    code i did two years ago

--- generate_character_stats.php
+++ generate_character_stats.php
@@ -7,19 +7,19 @@ $name = filter_var($_POST['character_name'], FILTER_SANITIZE_STRING);
 $selection = 'SELECT * FROM characters WHERE name = "'.$name.'"';
 $result = mysqli_query($con,$selection);
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-
-echo '<div class="container-fluid">';
-echo '<div class="row" id="character-modal-firstrow">';
-	echo '<div class="col-xs-2" id="character-modal-sprite">';
-		echo '<img src="',$row['sprite'],'" title="',$row['name'],'"/>';
-	echo '</div>';
-	echo '<div class="col-xs-10" id="character-modal-asl">';
-		echo '<span id="character-modal-gender" value="',$row['gender'],'"></span><br>';
-		echo '<span id="character-modal-nation" value="',$row['nation'],'">',$row['nation'],'</span><br>';
-		echo 'Hometown: ',$row['location'],'';
-	echo '</div>';
-echo '</div>';
-
+?>
+<div class="container-fluid">
+<div class="row" id="character-modal-firstrow">
+	<div class="col-xs-2" id="character-modal-sprite">
+		<img src="<?php echo $row['sprite']; ?>" title="<?php echo $row['name']; ?>"/>
+	</div>
+	<div class="col-xs-10" id="character-modal-asl">
+		<span id="character-modal-gender" value="<?php echo $row['gender'] ?>"></span><br>
+		<span id="character-modal-nation" value="<?php echo $row['nation']; ?>"><?php echo $row['nation']; ?></span><br>
+		Hometown: <?php echo $row['location']; ?>
+	</div>
+</div>
+<?php
 $hp = $row['hp_rating'];
 $sp = $row['sp_rating'];
 $str = $row['str_rating'];
@@ -90,6 +90,8 @@ elseif ($agi == 'D')
 	$agi = ' 5,9.5';
 elseif ($agi == 'E')
 	$agi = ' 6,9';
+elseif ($agi == 'S')
+	$agi = ' 1,11.5';
 
 if ($int == 'A')
 	$int = ' 2,5';
@@ -101,77 +103,61 @@ elseif ($int == 'D')
 	$int = ' 5,6.5';
 elseif ($int == 'E')
 	$int = ' 6,7';
+?>
+<div class="row" id="character-modal-secondrow">
+	<div id="svgcol">
+		<div id="character-modal-backing-hex">
+			<svg xmlns="http://www.w3.org/2000/svg"
+		    xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="none" style="width:100%; height:100%" viewBox="0 0 16 16">
 
-echo '<div class="row" id="character-modal-secondrow">';
-	echo '<div id="svgcol">';
-	echo '<div id="character-modal-backing-hex">';
-	echo '<svg xmlns="http://www.w3.org/2000/svg"
-    xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="none" style="width:100%; height:100%" viewBox="0 0 16 16">
+			<text x="8" y="1.5" font-size="0.9" stroke="black" stroke-width="0.01" text-anchor="middle" fill="black">HP</text>
+			<text x="15" y="4.7" font-size="0.9" stroke="black" stroke-width="0.01" text-anchor="middle" fill="black">SP</text>
+			<text x="15" y="11.5" font-size="0.9" stroke="black" stroke-width="0.01" text-anchor="middle" fill="black">STR</text>
+			<text x="8" y="15" font-size="0.9" stroke="black" stroke-width="0.01" text-anchor="middle" fill="black">DEX</text>
+			<text x="0.8" y="11.5" font-size="0.9" stroke="black" stroke-width="0.01" text-anchor="middle" fill="black">AGI</text>
+			<text x="0.8" y="4.7" font-size="0.9" stroke="black" stroke-width="0.01" text-anchor="middle" fill="black">INT</text>
 
-	<text x="8" y="1.5" font-size="0.9" stroke="black" stroke-width="0.01" text-anchor="middle" fill="black">HP</text>
+		    <polygon points="8,2 14,5 14,11 8,14 2,11 2,5"
+				style="stroke:#777777; fill:none; stroke-width: 0.07"/>
 
-	<text x="15" y="4.7" font-size="0.9" stroke="black" stroke-width="0.01" text-anchor="middle" fill="black">SP</text>
+			<polygon points="8,3 13,5.5 13,10.5 8,13 3,10.5 3,5.5"
+				style="stroke:#777777; fill:none; stroke-width: 0.07"/>
 
-	<text x="15" y="11.5" font-size="0.9" stroke="black" stroke-width="0.01" text-anchor="middle" fill="black">STR</text>
+			<polygon points="8,4 12,6 12,10 8,12 4,10 4,6"
+				style="stroke:#777777; fill:none; stroke-width: 0.07"/>
 
-	<text x="8" y="15" font-size="0.9" stroke="black" stroke-width="0.01" text-anchor="middle" fill="black">DEX</text>
+			<polygon points="8,5 11,6.5 11,9.5 8,11 5,9.5 5,6.5"
+				style="stroke:#777777; fill:none; stroke-width: 0.07"/>
 
-	<text x="0.8" y="11.5" font-size="0.9" stroke="black" stroke-width="0.01" text-anchor="middle" fill="black">AGI</text>
+			<polygon points="8,6 10,7 10,9 8,10 6,9 6,7"
+				style="stroke:#777777; fill:none; stroke-width: 0.07"/>
 
-	<text x="0.8" y="4.7" font-size="0.9" stroke="black" stroke-width="0.01" text-anchor="middle" fill="black">INT</text>
+			<polygon points="8,7 9,7.5 9,8.5 8,9 7,8.5 7,7.5"
+				style="stroke:#777777; fill:none; stroke-width: 0.07"/>
 
-    <polygon points="8,2 14,5 
-  					14,11 8,14
-                   2,11 2,5"
-		style="stroke:#777777; fill:none; stroke-width: 0.07"/>
+			<line x1="8" x2="8" y1="2" y2="14" style="stroke:#777777; fill:none; stroke-width: 0.07"/>
 
-	<polygon points="8,3 13,5.5 
-  					13,10.5 8,13
-                   3,10.5 3,5.5"
-		style="stroke:#777777; fill:none; stroke-width: 0.07"/>
+			<line x1="2" x2="14" y1="5" y2="11" style="stroke:#777777; fill:none; stroke-width: 0.07"/>
 
-	<polygon points="8,4 12,6 
-  					12,10 8,12
-                   4,10 4,6"
-		style="stroke:#777777; fill:none; stroke-width: 0.07"/>
+			<line x1="2" x2="14" y1="11" y2="5" style="stroke:#777777; fill:none; stroke-width: 0.07"/>
+			<?php if (strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') !== FALSE || strpos($_SERVER['HTTP_USER_AGENT'], 'Safari') !== FALSE) { ?>
+				<polygon points="8,8 8,8 8,8 8,8 8,8 8,8" style="stroke:#333333; fill:<?php echo $statcolor; ?>; opacity: 0.5; stroke-width: 0.1">
+				<animate id="animation" attributeName="points" attributeType="XML" to="<?php echo $hp,$sp,$str,$dex,$agi,$int; ?>" 
+						begin="0.1s" calcMode="spline" keyTimes="0;0.9" keySplines="0.42, 0, 0.9, 0.9" dur="0.9s" fill="freeze" /></polygon>
+			<?php } else { ?>
+				<polygon points="<?php echo $hp,$sp,$str,$dex,$agi,$int; ?>" style="stroke:#333333; fill:<?php echo $statcolor; ?>; opacity: 0.5; stroke-width: 0.1">
+				</polygon>
+			<?php } ?>
+			</svg>
+		</div>
+	</div>
+</div>
 
-	<polygon points="8,5 11,6.5 
-  					11,9.5 8,11
-                   5,9.5 5,6.5"
-		style="stroke:#777777; fill:none; stroke-width: 0.07"/>
+<div class="row" id="character-modal-thirdrow">
+<?php echo $row['description']; ?>
+</div>
 
-	<polygon points="8,6 10,7 
-  					10,9 8,10
-                   6,9 6,7"
-		style="stroke:#777777; fill:none; stroke-width: 0.07"/>
-
-	<polygon points="8,7 9,7.5 
-  					9,8.5 8,9
-                   7,8.5 7,7.5"
-		style="stroke:#777777; fill:none; stroke-width: 0.07"/>
-
-	<line x1="8" x2="8" y1="2" y2="14" style="stroke:#777777; fill:none; stroke-width: 0.07"/>
-
-	<line x1="2" x2="14" y1="5" y2="11" style="stroke:#777777; fill:none; stroke-width: 0.07"/>
-
-	<line x1="2" x2="14" y1="11" y2="5" style="stroke:#777777; fill:none; stroke-width: 0.07"/>';
-
-	echo '<polygon points="8,8 8,8 8,8 8,8 8,8 8,8" style="stroke:#333333; fill:',$statcolor,'; opacity: 0.5; stroke-width: 0.1">';
-	echo '<animate id="animation" attributeName="points" attributeType="XML" to="',$hp,$sp,$str,$dex,$agi,$int,'" 
-			begin="0.1s" calcMode="spline" keyTimes="0;.9" keySplines=".42 0 .9 .9" dur=".9s" fill="freeze" /></polygon>';
-	echo '</svg>';
-	echo '</div>';
-	echo '</div>';
-echo '</div>';
-
-echo '<div class="row" id="character-modal-thirdrow">';
-echo $row['description'];
-echo '</div>';
-
-echo '</div>';
-
-
-
+</div>
+<?php
 mysqli_free_result($result);
-
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+require_once('config.php');
+?>
+
 <!DOCTYPE html>
 <htmL>
 <head>
@@ -8,7 +12,7 @@
     <link href='https://fonts.googleapis.com/css?family=Raleway:200,400,700' rel='stylesheet' type='text/css'>
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/database.css" rel="stylesheet">
-
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.js"></script>
 </head>
 <body>
 	<!-- Navigation bar on top of window -->
@@ -27,12 +31,13 @@
           <ul class="nav navbar-nav navbar-right">
             <li class="dropdown active"> 
             	<a id="dropdown-characters" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> 
-            	Characters <span class="caret"></span></a> 
+            		Characters <span class="caret"></span>
+            	</a> 
         		<ul class="dropdown-menu" aria-labelledby="dropdown-characters"> 
-        		<li id="navbar-playable" class="active"><a href="#">Playable Characters</a></li> 
-        		<li id="navbar-npcs"><a href="#">Antagonists and NPCs</a></li> 
-        		<li id="navbar-statistics"><a href="#">Statistics (coming soon!)</a></li> 
-        		<li id="navbar-compare"><a href="#">Compare Characters (coming soon!)</a></li> 
+	        		<li id="navbar-playable" class="active"><a href="#">Playable Characters</a></li> 
+	        		<li id="navbar-npcs"><a href="#">Antagonists and NPCs</a></li> 
+	        		<li id="navbar-statistics"><a href="#">Statistics</a></li> 
+	        		<li id="navbar-compare"><a href="#">Compare Characters (coming soon!)</a></li> 
         		</ul>
     		</li>
             <li id="navbar-locations"><a href="#">Locations (coming soon!)</a></li>
@@ -44,122 +49,115 @@
 
     <!-- Sidebar -->
     <div class="container-fluid">
-      <div class="row">
-        <div class="col-sm-3 col-md-2 sidebar">
-          <ul class="nav nav-sidebar">
-            <li id="sidebar-playable" class="active"><a href="#">Playable Characters <span class="sr-only">(current)</span></a></li>
-            <li id="sidebar-npcs"><a href="#">Antagonists and NPCs</a></li>
-            <li id="sidebar-statistics"><a href="#">Statistics (coming soon!)</a></li>
-            <li id="sidebar-compare"><a href="#">Compare Characters (coming soon!)</a></li>
-          </ul>
-        </div>
+    	<div class="row">
+       		<div class="col-sm-3 col-md-2 sidebar">
+       			<ul class="nav nav-sidebar">
+            		<li id="sidebar-playable" class="active"><a href="#">Playable Characters <span class="sr-only">(current)</span></a></li>
+            		<li id="sidebar-npcs"><a href="#">Antagonists and NPCs</a></li>
+            		<li id="sidebar-statistics"><a href="#">Statistics</a></li>
+            		<li id="sidebar-compare"><a href="#">Compare Characters (coming soon!)</a></li>
+          		</ul>
+        	</div>
+			<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+				<div class="row placeholders">
+            		<h1 class="sub-header">Playable Characters</h1>
+					<div class="container-fluid">
+  						<div class="row">
+	  						<div class="panel-group" id="accordion" role="tablist">
+				  				<div class="panel panel-default">
+				    				<div class="panel-heading" role="tab" id="headingOne">
+				      					<h3 class="panel-title">
+				        					<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+				          						Search
+				        					</a>
+				      					</h3>
+				    				</div>
+				    				<div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+				      					<div class="panel-body">
+									  	<?php
+											// Selecting by gender
+											echo '<div class="form-group"><label for="select-gender">Gender</label><select class="form-control" id="select-gender"><option></option>';
+											$selection3 = "SELECT DISTINCT gender FROM characters ORDER BY gender";
+											$result3 = mysqli_query($con, $selection3);
+											$row3 = mysqli_fetch_array($result3, MYSQLI_ASSOC);
+												echo '<option>', $row3["gender"], '</option>';
+											while ($row3 = mysqli_fetch_array($result3, MYSQLI_ASSOC)) {
+												echo '<option>', $row3["gender"], '</option>';
+											}
+											echo '</select></div>';
 
-        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+											// Selecting by nation
+											echo '<div class="form-group"><label for="select-nation">Nation</label><select class="form-control" id="select-nation"><option></option>';
+											$selection = "SELECT DISTINCT nation FROM characters ORDER BY nation";
+											$result = mysqli_query($con,$selection);
+											$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+												echo '<option>', $row["nation"], '</option>';
+											while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+												echo '<option>', $row["nation"], '</option>';
+											}
+											echo '</select></div>';
 
-        <div class="row placeholders">
-            
+											// Selecting by location
+											echo '<div class="form-group"><label for="select-location">Location</label><select class="form-control" id="select-location"><option></option>';
+											$selection2 = "SELECT DISTINCT location FROM characters ORDER BY location";
+											$result2 = mysqli_query($con, $selection2);
+											$row2 = mysqli_fetch_array($result2, MYSQLI_ASSOC);
+												echo '<option>', $row2["location"], '</option>';
+											while ($row2 = mysqli_fetch_array($result2, MYSQLI_ASSOC)) {
+												echo '<option>', $row2["location"], '</option>';
+											}
+											echo '</select></div>';
 
-          <h1 class="sub-header">Playable Characters</h1>
-          <div class="container-fluid">
-  			<div class="row">
-	  			<div class="panel-group" id="accordion" role="tablist">
-				  <div class="panel panel-default">
-				    <div class="panel-heading" role="tab" id="headingOne">
-				      <h3 class="panel-title">
-				        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-				          Search
-				        </a>
-				      </h3>
-				    </div>
-				    <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
-				      <div class="panel-body">
-				      		
-					  	<?php
-
-						require_once('config.php');
-
-						// Selecting by gender
-						echo '<div class="form-group"><label for="select-gender">Gender</label><select class="form-control" id="select-gender"><option></option>';
-						$selection3 = "SELECT DISTINCT gender FROM characters ORDER BY gender";
-						$result3 = mysqli_query($con,$selection3);
-						$row3 = mysqli_fetch_array($result3,MYSQLI_ASSOC);
-							echo '<option>',$row3["gender"],'</option>';
-						while($row3 = mysqli_fetch_array($result3,MYSQLI_ASSOC)) {
-							echo '<option>',$row3["gender"],'</option>';
-						}
-						echo '</select></div>';
-
-						// Selecting by nation
-						echo '<div class="form-group"><label for="select-nation">Nation</label><select class="form-control" id="select-nation"><option></option>';
-						$selection = "SELECT DISTINCT nation FROM characters ORDER BY nation";
-						$result = mysqli_query($con,$selection);
-						$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-							echo '<option>',$row["nation"],'</option>';
-						while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
-							echo '<option>',$row["nation"],'</option>';
-						}
-						echo '</select></div>';
-
-						// Selecting by location
-						echo '<div class="form-group"><label for="select-location">Location</label><select class="form-control" id="select-location"><option></option>';
-						$selection2 = "SELECT DISTINCT location FROM characters ORDER BY location";
-						$result2 = mysqli_query($con,$selection2);
-						$row2 = mysqli_fetch_array($result2,MYSQLI_ASSOC);
-							echo '<option>',$row2["location"],'</option>';
-						while($row2 = mysqli_fetch_array($result2,MYSQLI_ASSOC)) {
-							echo '<option>',$row2["location"],'</option>';
-						}
-						echo '</select></div>';
-
-						mysqli_free_result($result);
-						mysqli_free_result($result2);
-						mysqli_free_result($result3);
-						?>
-						<!-- Sorting method -->
-						<div class="form-group"><label for="sort-by">Sort By</label><select class="form-control" id="sort-by">
-						<option>name</option>
-						<option>hp_rating</option>
-						<option>sp_rating</option>
-						<option>str_rating</option>
-						<option>dex_rating</option>
-						<option>agi_rating</option>
-						<option>int_rating</option>
-						</select></div>
-
-						<button type="button" class="btn btn-default btn-lg" id="search-button">
-						  <span class="glyphicon glyphicon-search" aria-hidden="true"></span> Search
-						</button>
-				      </div>
-				    </div>
-				  </div>
-				  </div>
-			</div>
-			</div>
+											mysqli_free_result($result);
+											mysqli_free_result($result2);
+											mysqli_free_result($result3);
+										?>
+											<!-- Sorting method -->
+											<div class="form-group"><label for="sort-by">Sort By</label><select class="form-control" id="sort-by">
+												<option>name</option>
+												<option>hp_rating</option>
+												<option>sp_rating</option>
+												<option>str_rating</option>
+												<option>dex_rating</option>
+												<option>agi_rating</option>
+												<option>int_rating</option>
+												</select>
+											</div>
+											<button type="button" class="btn btn-default btn-lg" id="search-button">
+												<span class="glyphicon glyphicon-search" aria-hidden="true"></span> Search
+											</button>
+				      					</div>
+				    				</div>
+				  				</div>
+				  			</div>
+						</div>
+					</div>
 
 			<!-- Creating the grid of characters -->
 			<div id="character-grid-container">
-			<div class="character-grid">
-			<?php
-				$selection = "SELECT name,static_sprite FROM characters ORDER BY name";
-				$result = mysqli_query($con,$selection);
-				
-				do {
-					echo '<div class="row">';
-					for ($x = 0; $x <= 3; $x++) {
-						$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-						if ($row != NULL) {
-							echo '<div class="col-xs-3 character character-playable" style="text-align:center" data-toggle="modal" data-target="#characterDetails">';
-							echo '<h4 value="',$row["name"],'">',$row["name"],'</h4>';
-							echo '<img src="',$row["static_sprite"],'" />';
-							echo '</div>';
+				<div class="character-grid">
+				<?php
+					$selection = "SELECT name,static_sprite FROM characters ORDER BY name";
+					$result = mysqli_query($con, $selection);
+					do {
+						echo '<div class="row">';
+						for ($x = 0; $x <= 3; $x++) {
+							$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+							if ($row != NULL) { 
+							?>
+								<div class="col-xs-3 character character-playable" style="text-align:center" data-toggle="modal" data-target="#characterDetails">
+									<h4 value="<?php echo $row["name"]; ?>"><?php echo $row["name"]; ?></h4>
+									<img src="<?php echo $row["static_sprite"]; ?>" />
+								</div>
+							<?php
+							}
 						}
-					}
-					echo '</div>';
-				} while($row != NULL);
-				
-				mysqli_free_result($result);
-			?>
-			</div></div>
+						echo '</div>';
+					} while($row != NULL);
+					mysqli_free_result($result);
+				?>
+				</div>
+			</div>
 		</div>
 		</div>
 		</div>
@@ -208,6 +206,7 @@
 		      a chronological account of the ideas and emotions I had put into it over the years.</p>
 		      <p>Why did I make a database for an unfinished game? Because I can, I guess.</p>
 		      <p>This website was developed from scratch using HTML with SVG animations, CSS, JavaScript, jQuery, AJAX, PHP, MySQL, and the Bootstrap framework for a responsive, mobile-friendly design. I spent 4 days working on it.</p>
+		      <p><i>September 18, 2017: Added statistics tab. Chart generated via chart.js</i></p>
 		      </div>
 		      <div class="modal-footer">
 		        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -221,12 +220,15 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.min.js"></script> -->
     <script src="js/script.js"></script>
 
     <script>
 	$(window).on("load", function() {
 		$('.character').each(function(index) {
-			$(this).delay(120*index).queue(function() { $(this).addClass('in').dequeue(); });
+			$(this).delay(120*index).queue(function() { 
+				$(this).addClass('in').dequeue(); 
+			});
 
 		});
 
